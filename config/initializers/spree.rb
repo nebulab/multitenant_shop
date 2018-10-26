7ef559106194d4ea602ba87ff7cc19da9d7529fe
Flavio Auciello <flavioauciello@nebulab.it>
2018-10-26 03:32:41 +0200

    Iterate over tenants to set Spree preferences
    now checking "Contact us at" section it should not display the
    correct tenant value, but the last to be defined (pescara_shop).

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,61 +1,47 @@
 # Configure Solidus Preferences
 # See http://docs.solidus.io/Spree/AppConfiguration.html for details
 
-Spree.config do |config|
-  # Core:
+Apartment::Tenant.switch(:latina_shop) do
+  Spree.config do |config|
+    # config.use_legacy_db_preferences!
 
-  # Default currency for new sites
-  config.currency = "USD"
+    # Default currency for new sites
+    config.currency = "EUR"
 
-  # from address for transactional emails
-  config.mails_from = "store@example.com"
+    # from address for transactional emails
+    config.mails_from = 'store@latina-shop.com'
+  end
 
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+  Spree::Frontend::Config.configure do |config|
+    config.locale = 'it'
+  end
 
-  # When set, product caches are only invalidated when they fall below or rise
-  # above the inventory_cache_threshold that is set. Default is to invalidate cache on
-  # any inventory changes.
-  # config.inventory_cache_threshold = 3
-
-
-  # Frontend:
-
-  # Custom logo for the frontend
-  # config.logo = "logo/solidus_logo.png"
-
-  # Template to use when rendering layout
-  # config.layout = "spree/layouts/spree_application"
-
-
-  # Admin:
-
-  # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus_logo.png"
-
-  # Gateway credentials can be configured statically here and referenced from
-  # the admin. They can also be fully configured from the admin.
-  #
-  # config.static_model_preferences.add(
-  #   Spree::Gateway::StripeGateway,
-  #   'stripe_env_credentials',
-  #   secret_key: ENV['STRIPE_SECRET_KEY'],
-  #   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  #   server: Rails.env.production? ? 'production' : 'test',
-  #   test_mode: !Rails.env.production?
-  # )
+  Spree::Backend::Config.configure do |config|
+    config.locale = 'it'
+  end
 end
 
-Spree::Frontend::Config.configure do |config|
-  config.locale = 'en'
-end
+Apartment::Tenant.switch(:pescara_shop) do
+  Spree.config do |config|
+    # config.use_legacy_db_preferences!
 
-Spree::Backend::Config.configure do |config|
-  config.locale = 'en'
+    # Default currency for new sites
+    config.currency = "USD"
+
+    # from address for transactional emails
+    config.mails_from = 'store@pescara-shop.com'
+  end
+
+  Spree::Frontend::Config.configure do |config|
+    config.locale = 'en'
+  end
+
+  Spree::Backend::Config.configure do |config|
+    config.locale = 'en'
+  end
 end
 
 Spree::Api::Config.configure do |config|
   config.requires_authentication = true
 end
 
-Spree.user_class = "Spree::LegacyUser"
